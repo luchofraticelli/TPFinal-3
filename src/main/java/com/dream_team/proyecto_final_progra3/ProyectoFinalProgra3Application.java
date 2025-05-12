65473112f4ab53d23308bf74eb9c7f0e1cd791a5
Emiliano <emilianogbarrientos@gmail.com>
2025-05-12 13:17:02 -0300

    iniciando el proyecto;

---
+++ src/main/java/com/dream_team/proyecto_final_progra3/ProyectoFinalProgra3Application.java
@@ -0,0 +1,13 @@
+package com.dream_team.proyecto_final_progra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalProgra3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalProgra3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream_team/proyecto_final_progra3/ProyectoFinalProgra3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream_team.proyecto_final_progra3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalProgra3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
